3a8f9eb983e5d726a08d09b0d44a0a7dc9f75290
MR-Stan <michaelrstanley87@gmail.com>
2020-02-08 18:26:59 -0600

    removed unnecessary files, installed reactstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Table
+} from 'reactstrap';
+import './App.css';
+
+class App extends Component {
+  state = {
+
+  }
+}
+
+
+
+
+export default App;
