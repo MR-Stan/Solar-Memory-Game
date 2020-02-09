672c9951dd3915e338432898a986f912f1bfaaa5
MR-Stan <michaelrstanley87@gmail.com>
2020-02-09 12:44:00 -0600

    Updates

---
+++ precache-manifest.cdcb518cfd9256f8d9aabf962cc6e55f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3041faaa164759fc568c567296039429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a94dafa4aad0d6e43c4b",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c7b2181487b79b291f79",
+    "url": "/static/css/main.4907f426.chunk.css"
+  },
+  {
+    "revision": "a94dafa4aad0d6e43c4b",
+    "url": "/static/js/2.e21e55f1.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.e21e55f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b2181487b79b291f79",
+    "url": "/static/js/main.008c713c.chunk.js"
+  },
+  {
+    "revision": "cc2927c6818dc6c4f663",
+    "url": "/static/js/runtime-main.f75059d1.js"
+  },
+  {
+    "revision": "e9eeb12c20c774d102a7ca5aa11163dc",
+    "url": "/static/media/ceres.e9eeb12c.jpg"
+  },
+  {
+    "revision": "8d4396326e987d17d1235226c23579d3",
+    "url": "/static/media/earth.8d439632.jpg"
+  },
+  {
+    "revision": "61d6628840e220649f71cc6d4fac7996",
+    "url": "/static/media/eris.61d66288.jpg"
+  },
+  {
+    "revision": "72ab2d0483412fa4d84df489da3b4b0a",
+    "url": "/static/media/haumea.72ab2d04.jpg"
+  },
+  {
+    "revision": "2e905f7af0dc4cd96ceeefa92d0db6ac",
+    "url": "/static/media/jupiter.2e905f7a.jpg"
+  },
+  {
+    "revision": "641158a509519b0e126a17644e7d938a",
+    "url": "/static/media/luna.641158a5.jpg"
+  },
+  {
+    "revision": "0e95b74982af3f37471d0511e9259cb6",
+    "url": "/static/media/makemake.0e95b749.jpg"
+  },
+  {
+    "revision": "b4cff7b8abab6bc9b8bae6cf560997e9",
+    "url": "/static/media/mars.b4cff7b8.jpg"
+  },
+  {
+    "revision": "bffdfa5ab9d7c2facbfb15618ec3da5c",
+    "url": "/static/media/mercury.bffdfa5a.jpg"
+  },
+  {
+    "revision": "7d982ad00c0d2d39e9b526ceead3d3fe",
+    "url": "/static/media/neptune.7d982ad0.jpg"
+  },
+  {
+    "revision": "50a624493ef60b5998c8fd1871cec97b",
+    "url": "/static/media/pluto.50a62449.jpg"
+  },
+  {
+    "revision": "5bb42c6a61718b93152cb1eefb31c1bf",
+    "url": "/static/media/saturn.5bb42c6a.jpg"
+  },
+  {
+    "revision": "1efda266785ac44d5201ba44c5b3846a",
+    "url": "/static/media/sun.1efda266.jpg"
+  },
+  {
+    "revision": "174e0d551c810d1b8bc0f903d2b04df7",
+    "url": "/static/media/uranus.174e0d55.jpg"
+  },
+  {
+    "revision": "4ff599b4bea860bcdf2e2163d9700fb6",
+    "url": "/static/media/venus.4ff599b4.jpg"
+  }
+]);
