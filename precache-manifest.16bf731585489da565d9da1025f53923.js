2288ed3d60a4e9dc5b00824d16f773adc4c08003
MR-Stan <michaelrstanley87@gmail.com>
2020-02-09 17:58:46 -0600

    Updates

---
+++ precache-manifest.16bf731585489da565d9da1025f53923.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41db0529f5016d9e108f97cf7a1a10a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "5e3e50be399ca22bf724",
+    "url": "/Clicky-Game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1107ed40bcd2e02ef14e",
+    "url": "/Clicky-Game/static/css/main.0a7e68ac.chunk.css"
+  },
+  {
+    "revision": "5e3e50be399ca22bf724",
+    "url": "/Clicky-Game/static/js/2.cbe511fc.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Clicky-Game/static/js/2.cbe511fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1107ed40bcd2e02ef14e",
+    "url": "/Clicky-Game/static/js/main.e94b87ce.chunk.js"
+  },
+  {
+    "revision": "ed33e96b23e5ad0b910f",
+    "url": "/Clicky-Game/static/js/runtime-main.da6617cf.js"
+  },
+  {
+    "revision": "9bcb5e5a004ccae00c09bd2842522982",
+    "url": "/Clicky-Game/static/media/ceres.9bcb5e5a.jpg"
+  },
+  {
+    "revision": "92dce846b2b5f2778a32ebb7188d76df",
+    "url": "/Clicky-Game/static/media/earth.92dce846.jpg"
+  },
+  {
+    "revision": "37643780c724f61aca2947672d7fa8b0",
+    "url": "/Clicky-Game/static/media/eris.37643780.jpg"
+  },
+  {
+    "revision": "1bec68bacb8687da7341c90da6056516",
+    "url": "/Clicky-Game/static/media/haumea.1bec68ba.jpg"
+  },
+  {
+    "revision": "ae13ff67ee4717a5ada9ca87a5126757",
+    "url": "/Clicky-Game/static/media/jupiter.ae13ff67.jpg"
+  },
+  {
+    "revision": "8d02df8e4fdd33dee984c6b3984eab85",
+    "url": "/Clicky-Game/static/media/luna.8d02df8e.jpg"
+  },
+  {
+    "revision": "0d30febc86cdace6bc60f2d1280b1bbd",
+    "url": "/Clicky-Game/static/media/makemake.0d30febc.jpg"
+  },
+  {
+    "revision": "3ee6a1fe58514890dd1ac2257fad8782",
+    "url": "/Clicky-Game/static/media/mars.3ee6a1fe.jpg"
+  },
+  {
+    "revision": "ddf30d51016b098a7c41b2f4b55b473c",
+    "url": "/Clicky-Game/static/media/mercury.ddf30d51.jpg"
+  },
+  {
+    "revision": "a765b2379a1b4a4f887f4ee392582257",
+    "url": "/Clicky-Game/static/media/milkyway.a765b237.jpg"
+  },
+  {
+    "revision": "4fb8677f0e81ec315b205624c60fcde5",
+    "url": "/Clicky-Game/static/media/neptune.4fb8677f.jpg"
+  },
+  {
+    "revision": "9d0db0becfefcb2fa673ac7e2e881ccd",
+    "url": "/Clicky-Game/static/media/pluto.9d0db0be.jpg"
+  },
+  {
+    "revision": "321278aecc133a566526d5ee8e39590e",
+    "url": "/Clicky-Game/static/media/saturn.321278ae.jpg"
+  },
+  {
+    "revision": "1063e31ea097ad0cc0e53d617e01b6f8",
+    "url": "/Clicky-Game/static/media/sun.1063e31e.jpg"
+  },
+  {
+    "revision": "e47676a06a089411bd5c370b5328cfa4",
+    "url": "/Clicky-Game/static/media/uranus.e47676a0.jpg"
+  },
+  {
+    "revision": "8899383b74dfc02a3f9483d9c16b8b37",
+    "url": "/Clicky-Game/static/media/venus.8899383b.jpg"
+  }
+]);
